c9e07cdbe94af1857d745f79b4326382a7b45493
Kirsten Smith <kirstenalexis7@gmail.com>
2023-12-18 16:36:53 -0800

    test 2 pass

--- src/wd-calc.js
+++ src/wd-calc.js
@@ -3,5 +3,8 @@ export default function WeekdayObj(userInput) {
 }
 
 WeekdayObj.prototype.weekdayFinder = function () {
-
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let date = new Date(this.userInput);
+  let weekdayNumber = date.getDay();
+  this.weekday = dayNames[weekdayNumber];
 }
